0a5842db6935e490ef740ffd56329ed0cb7ea3b5
Gotheru <gotheru@gmail.com>
2024-06-09 03:02:08 +0200

    fixed source in leftist heap

--- Implementations/content/data-structures/LeftistHeap.h
+++ Implementations/content/data-structures/LeftistHeap.h
@@ -1,27 +1,33 @@
+#pragma once
+
 /**
  * Description: Persistent meldable heap.
  * Time: O(\log N) per meld
  * Memory: O(\log N) per meld
  * Source:
- 	* https://judge.yosupo.jp/submission/11843
+ 	* nor (https://judge.yosupo.jp/submission/87297)
  	* https://en.wikipedia.org/wiki/Leftist_tree
  * Verification: See Kth Walk
  */
 
-typedef pair<ll,int> T;
-typedef struct heap* ph;
-struct heap { // min heap
-	ph l = NULL, r = NULL;
-	int s = 0; T v; // s: path to leaf
-	heap(T _v):v(_v) {}
-};
+#include "../contest/template.hpp"
 
-ph meld(ph p, ph q) {
-	if (!p || !q) return p?:q;
-	if (p->v > q->v) swap(p,q);
-	ph P = new heap(*p); P->r = meld(P->r,q);
-	if (!P->l || P->l->s < P->r->s) swap(P->l,P->r);
-	P->s = (P->r?P->r->s:0)+1; return P;
-}
-ph ins(ph p, T v) { return meld(p, new heap(v)); }
-ph pop(ph p) { return meld(p->l,p->r); }
+tcTU> struct LeftistHeap {
+    using self_t = LeftistHeap<T, U>;
+    int rank; T key; U value;
+    self_t *left, *right;
+    LeftistHeap(int rank_, T key_, U value_, self_t* left_,
+                self_t* right_)
+        : rank{rank_}, key{key_}, value{value_}, left{left_}, right{right_} {}
+    inline static deque<LeftistHeap> alloc;
+    static self_t* insert(LeftistHeap* a, const T k, const U v) {
+        if (not a or k < a->key) {
+            alloc.emplace_back(1, k, v, a, nullptr);
+            return &alloc.back();
+        }
+        auto l = a->left, r = insert(a->right, k, v);
+        if (not l or r->rank > l->rank) swap(l, r);
+        alloc.emplace_back(r ? r->rank + 1 : 0, a->key, a->value, l, r);
+        return &alloc.back();
+    }
+};
